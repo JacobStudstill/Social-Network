0b0e49a52ce849f367bb730c39d9681d8eaec959
Jacob Studstill <jacob.studstill@gmail.com>
2023-03-01 15:53:51 -0500

    Adding the thought-routes with completed code

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction,
+} = require('../../controllers/courseController.js');
+
+// /api/courses
+router.route('/').get(getCourses).post(createCourse);
+
+// /api/courses/:courseId
+router
+  .route('/:courseId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+module.exports = router;
